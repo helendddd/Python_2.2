bac6fa87c6997c18800e38aa674bcb1e1495e068
helendddd <mikheeva.el.24@gmail.com>
2023-12-13 22:04:43 +0300

    fix individual 3

--- individual_3.py
+++ individual_3.py
@@ -6,15 +6,13 @@
 # Определить, сколько будет клеток через 6 часов.
 
 
-def ameba(n, hours):
-    while hours >= 3:
-        n += n
-        hours -= 3
-    return n
-
-
 if __name__ == '__main__':
     n = int(input("Enter the number of amebas... "))
     hours = int(input("Enter the hours... "))
-    print(f"The number of {n} amoebas after {hours} hours is", end=" ")
-    print(ameba(n, hours))
+    result = n
+    time = hours
+    while time >= 3:
+        result += n
+        time -= 3
+
+    print(f"The number of {n} amoebas after {hours} hours is {result}")
